ef238aed23c0e313aec1c1ba9474babc2ce36e44
nguyenquocbao123 <70358069+nguyenquocbao123@users.noreply.github.com>
2020-08-28 10:51:48 +0700

    commit

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -5,16 +5,7 @@ import 'form_email.dart';
 
 class SignIn extends StatefulWidget {
   SignIn({Key key, this.title, String username}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+//check command github branch
   final String title;
 
   @override
